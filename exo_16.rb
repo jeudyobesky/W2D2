b7e26e20fa49d81f3cbd57c3933d5e6085bfbf33
jeudyobesky <jeudy.obesky@gmail.com>
2021-04-14 07:14:02 +0200

    review

--- exo_16.rb
+++ exo_16.rb
@@ -1,12 +1,16 @@
 print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 etage = gets.chomp.to_i
-number_of = 0
-cross = '#'
-cross_add = cross * number_of
-cross_array = ["Voici la pyramide"]
-while etage > number_of
-    number_of += 1
+if etage<1 or etage>25
+    puts "Un nombre entre 1 et 25"
+else
+    number_of = 0
+    cross = '#'
     cross_add = cross * number_of
-    cross_array << "#{cross_add}"
+    cross_array = ["Voici la pyramide"]
+    while etage > number_of
+        number_of += 1
+        cross_add = cross * number_of
+        cross_array << "#{cross_add}"
+    end
 end
-puts cross_array
+puts cross_array.reverse
